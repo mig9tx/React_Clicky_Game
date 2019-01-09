88e58d0f4b72830ed141c4ca783e4c29634d7126
mig9tx <migue.trujillo@utexas.edu>
2019-01-09 00:21:57 -0600

    Updates

---
+++ precache-manifest.feaf549daf60e8d346ba31ac75566263.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45f6b6d0e7a60d90c63d",
+    "url": "/React_Clicky_Game/static/js/runtime~main.45f6b6d0.js"
+  },
+  {
+    "revision": "8029e7ef9e28df14a5b8",
+    "url": "/React_Clicky_Game/static/js/main.8029e7ef.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/React_Clicky_Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "8029e7ef9e28df14a5b8",
+    "url": "/React_Clicky_Game/static/css/main.8f3fdcbc.chunk.css"
+  },
+  {
+    "revision": "535febf061528495e00b62f4863d1276",
+    "url": "/React_Clicky_Game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React_Clicky_Game/my-app/precache-manifest.3173b1b23903ada26a8f7fa194e7b057.js"
+  "/React_Clicky_Game/precache-manifest.feaf549daf60e8d346ba31ac75566263.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/React_Clicky_Game/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/React_Clicky_Game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
